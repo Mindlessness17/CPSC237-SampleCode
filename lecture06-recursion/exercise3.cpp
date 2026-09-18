3c3ef0878908ec443926c76d83721ed76da221d7
Nicholas Bohn <103919230+Mindlessness17@users.noreply.github.com>
2026-09-18 17:46:36 +0000

    done

--- lecture06-recursion/exercise3.cpp
+++ lecture06-recursion/exercise3.cpp
@@ -3,8 +3,15 @@ using namespace std;
 
 void printUp(int n)
 {
-    //to do
-    cout << n;
+    if (n == 1)
+    {
+        cout << "1, ";
+    }
+    else
+    {
+        printUp(n - 1);
+        cout << n;
+    }
 }
 
 int main()
